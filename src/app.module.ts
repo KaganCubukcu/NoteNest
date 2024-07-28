03cff1e633265b039995f75e90c9d1aa34805911
Kağan Çubukçu <kagancancubukcu@gmail.com>
2024-07-28 11:45:53 +0300

    Refactored NotesModule to include NotesController and NotesService

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { NotesModule } from './notes/notes.module';
+import { NotesController } from './notes/notes.controller';
+import { NotesService } from './notes/notes.service';
 
 @Module({
   imports: [NotesModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, NotesController],
+  providers: [AppService, NotesService],
 })
 export class AppModule {}
